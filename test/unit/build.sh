7b0346457fda757c55486d810263bc28db19b35d
nakatani.sho <nakatani.sho@dena.jp>
2014-11-08 13:56:34 +0900

    generalized build.sh

--- test/unit/build.sh
+++ test/unit/build.sh
@@ -3,9 +3,13 @@
 proj_root_dir=../..
 gtest_dir=${proj_root_dir}/contrib/gtest
 gtest_src="${gtest_dir}/src/gtest-all.cc ${gtest_dir}/src/gtest_main.cc"
-smartdb_lib_src=${proj_root_dir}/lib/datastruct/Record.cpp
-smartdb_lib_test=${proj_root_dir}/test/unit/datastruct/RecordTest.cpp
+# smartdb_lib_src=${proj_root_dir}/lib/datastruct/Record.cpp
 target=run_gtest
+
+smartdb_lib_test=$(find . -name '*.cpp')
+
+_proj_root_dir_for_sed=$(echo $proj_root_dir |sed -e 's/\./\\./g' |sed -e 's/\//\\\//g')
+smartdb_lib_src=$(echo $smartdb_lib_test |sed -e "s/\.\//${_proj_root_dir_for_sed}\/lib\//g" |sed -e 's/Test\.cpp/.cpp/g')
 
 g++ \
     -g \
