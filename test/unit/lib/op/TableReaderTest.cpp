ada09ec2eeb0479c6378b7eeb40dcd63b20b0e97
nakatani.sho <nakatani.sho@dena.jp>
2014-11-22 18:11:45 +0900

    update

--- test/unit/lib/op/TableReaderTest.cpp
+++ test/unit/lib/op/TableReaderTest.cpp
@@ -14,25 +14,6 @@ protected:
   }
 };
 
-TEST_F(TableReaderTest, reads_from_csv) {
-  ColumnDef coldef("col1", SMARTDB_INT);
-  std::vector<const ColumnDef *> coldefs(1, &coldef);
-
-  std::unordered_map<std::string, std::string> extra = { {"path", "fixture/storage_csv_normal.csv"} };
-
-  TableReader op(coldefs, "csv", extra, 100);
-  EXPECT_EQ(NO_ERR, op.run());
-  while (!op.out_q.finished()) {
-    const Records *records = op.out_q.front();
-    EXPECT_NE((Records *)NULL, records);
-    EXPECT_EQ(3, records->size());
-
-    EXPECT_EQ(101, GET_SMARTDB_VALUE(records->columns[0]->get(0), SmartdbInt));
-    EXPECT_EQ(201, GET_SMARTDB_VALUE(records->columns[0]->get(1), SmartdbInt));
-    EXPECT_EQ(301, GET_SMARTDB_VALUE(records->columns[0]->get(2), SmartdbInt));
-    op.out_q.pop();
-  }
-}
 TEST_F(TableReaderTest, reads_from_csv_record_by_record) {
   ColumnDef coldef("col1", SMARTDB_INT);
   std::vector<const ColumnDef *> coldefs(1, &coldef);
@@ -49,6 +30,25 @@ TEST_F(TableReaderTest, reads_from_csv_record_by_record) {
     EXPECT_EQ(1, records->size());
 
     EXPECT_EQ(records_cnt * 100 + 1, GET_SMARTDB_VALUE(records->columns[0]->get(0), SmartdbInt));
+    op.out_q.pop();
+  }
+}
+TEST_F(TableReaderTest, reads_from_csv) {
+  ColumnDef coldef("col1", SMARTDB_INT);
+  std::vector<const ColumnDef *> coldefs(1, &coldef);
+
+  std::unordered_map<std::string, std::string> extra = { {"path", "fixture/storage_csv_normal.csv"} };
+
+  TableReader op(coldefs, "csv", extra, 100);
+  EXPECT_EQ(NO_ERR, op.run());
+  while (!op.out_q.finished()) {
+    const Records *records = op.out_q.front();
+    EXPECT_NE((Records *)NULL, records);
+    EXPECT_EQ(3, records->size());
+
+    EXPECT_EQ(101, GET_SMARTDB_VALUE(records->columns[0]->get(0), SmartdbInt));
+    EXPECT_EQ(201, GET_SMARTDB_VALUE(records->columns[0]->get(1), SmartdbInt));
+    EXPECT_EQ(301, GET_SMARTDB_VALUE(records->columns[0]->get(2), SmartdbInt));
     op.out_q.pop();
   }
 }
